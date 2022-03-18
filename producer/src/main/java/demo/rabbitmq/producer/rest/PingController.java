0a2fe86ad233c5b6e9b6bad234f51d9de32df0ff
Dmitry <dmitry-lt@users.noreply.github.com>
2022-03-18 15:46:37 +0300

    better logging

--- producer/src/main/java/demo/rabbitmq/producer/rest/PingController.java
+++ producer/src/main/java/demo/rabbitmq/producer/rest/PingController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletContextEvent;
 import java.time.LocalDateTime;
 
 @RestController
--- producer/src/main/java/demo/rabbitmq/producer/service/RabbitMqService.java
+++ producer/src/main/java/demo/rabbitmq/producer/service/RabbitMqService.java
@@ -4,8 +4,10 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class RabbitMqService {
     @Value("${rabbitmq.topic-exchange-name}")
     private String topicExchangeName;
@@ -17,6 +19,7 @@ public class RabbitMqService {
     private RabbitTemplate rabbitTemplate;
 
     public void sendMessage(String message) {
+        log.info("Sending message: " + message);
         rabbitTemplate.convertAndSend(topicExchangeName, routingKey, message);
     }
 }
