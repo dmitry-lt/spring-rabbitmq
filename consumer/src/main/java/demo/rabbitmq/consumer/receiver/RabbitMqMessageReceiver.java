47aa6fc10c5c12d73fc90f587cfde5c867a59102
Dmitry <dmitry-lt@users.noreply.github.com>
2022-03-18 15:29:33 +0300

    consumer

--- consumer/src/main/java/demo/rabbitmq/consumer/receiver/RabbitMqMessageReceiver.java
+++ consumer/src/main/java/demo/rabbitmq/consumer/receiver/RabbitMqMessageReceiver.java
@@ -1,10 +1,12 @@
 package demo.rabbitmq.consumer.receiver;
 
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class RabbitMqMessageReceiver {
     public void receiveMessage(String message) {
-        System.out.println("Message received: " + message);
+        log.info("Message received: " + message);
     }
 }
