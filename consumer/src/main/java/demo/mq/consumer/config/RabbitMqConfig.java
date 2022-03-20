afd924d9485c90f354c4c2c285ed585ce0d3b75f
Dmitry <dmitry-lt@users.noreply.github.com>
2022-03-20 12:10:27 +0300

    consumer: create queue if it doesn't exist

--- consumer/src/main/java/demo/mq/consumer/config/RabbitMqConfig.java
+++ consumer/src/main/java/demo/mq/consumer/config/RabbitMqConfig.java
@@ -2,6 +2,7 @@ package demo.mq.consumer.config;
 
 import demo.mq.consumer.receiver.MessageReceiver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -12,6 +13,11 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class RabbitMqConfig {
     private final AmqpProperties amqpProperties;
+
+    @Bean
+    Queue queue() {
+        return new Queue(amqpProperties.getQueueName(), false);
+    }
 
     @Bean
     SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
